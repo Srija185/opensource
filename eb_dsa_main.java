d4380c22a69a63b46ff302db76a06620c0557934
Srija185 <srijapapai04@gmail.com>
2024-11-12 10:37:18 +0530

    Create eb_dsa_main.java

---
+++ eb_dsa_main.java
@@ -0,0 +1,10 @@
+import.java.io.*;
+import.java.util.*;
+public class solution {
+  public static void main(String args[]) {
+    Scanner scanner = new scanner(System.in);
+    int T = scanner.nextInt();
+    for (int t = 0; t < T; t++) {
+      int N = scanner.nextInt();
+      int X = scanner.nextInt();
+         
